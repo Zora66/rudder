6194a2a8d4fef3a986a7b76d567acc49774cc3a6
Zora66 <75981287+Zora66@users.noreply.github.com>
2022-03-30 12:18:15 +0200

    Update rudder.install.sh

--- rudder.install.sh
+++ rudder.install.sh
@@ -6,7 +6,7 @@ echo "ADDING GPG KEY"
 echo "=================="
 wget --quiet -O /etc/apt/trusted.gpg.d/rudder_apt_key.gpg "https://repository.rudder.io/apt/rudder_apt_key.gpg"
 
-sleep(1)
+sleep 1
 clear
 
 echo "==========================="
@@ -14,7 +14,7 @@ echo "ADDING REPO TO SOURCE LIST"
 echo "==========================="
 echo "deb http://repository.rudder.io/apt/7.0/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 
-sleep(1)
+sleep 1
 clear
 
 echo "==========================="
@@ -23,7 +23,7 @@ echo "==========================="
 apt-get update
 
 
-sleep(1)
+sleep 1
 clear
 
 echo "==========================="
